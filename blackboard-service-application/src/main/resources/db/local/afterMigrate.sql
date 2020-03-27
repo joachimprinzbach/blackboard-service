3b3783f9d1a4d5e36d4488ae54f136154d1cc5bb
joachimprinzbach <joachim.prinzbach@gmail.com>
2020-03-27 08:31:53 +0100

    Add initial data to work with

---
+++ blackboard-service-application/src/main/resources/db/local/afterMigrate.sql
@@ -0,0 +1,5 @@
+INSERT INTO PROPERTY
+    (id, user_id, section, property_key, property_value)
+VALUES
+    (100, 'k000123', 'mysection', 'mykey', 'myvalue'),
+    (101, 'k000123', 'othersection', 'otherkey', 'othervalue');
